efdc55d512933eab455c169ffc782613e08beaa4
steve <steve3816811205@gmail.com>
2026-02-05 14:15:12 +0800

    增加對註冊資料的檢查

--- src/main/java/com/example/selfintro/dto/RegisterRequest.java
+++ src/main/java/com/example/selfintro/dto/RegisterRequest.java
@@ -3,12 +3,19 @@ package com.example.selfintro.dto;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
+import jakarta.validation.constraints.Size;
+import jakarta.validation.constraints.NotEmpty;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class RegisterRequest {
 
+    @NotEmpty(message = "Username is required")
+    @Size(min = 3, max = 20, message = "Username must be between 3 and 20 characters")
     private String username;
+
+    @NotEmpty(message = "Password is required")
+    @Size(min = 6, max = 20, message = "Password must be between 6 and 20 characters")
     private String password;
 }
